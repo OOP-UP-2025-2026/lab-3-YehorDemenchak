b14b2bc24e73964e59ef220d41290b8f01b44dc2
Yehor Demenchak <9129079@stud.op.edu.ua>
2025-10-15 19:19:14 +0200

    lab 3 is done

--- src/main/java/org/example/task2/Main.java
+++ src/main/java/org/example/task2/Main.java
@@ -2,6 +2,11 @@ package org.example.task2;
 
 public class Main {
     public static void main(String[] args) {
+        Box box = new Box(12.35, 36.6, 20.7);
+        System.out.println("Lateral surface area of the box: " + box.getLateralSurfaceArea());
+        System.out.println("Surface area of the box: " + box.getSurfaceArea());
+        System.out.println("Volume of the box: " + box.getVolume());
+        System.out.println();
 
         Cart cart = new Cart(new Item[10]);
         cart.add(new Item(1, "Samsung Galaxy S23", 27999));
@@ -14,15 +19,22 @@ public class Main {
         cart.add(new Item(8, "Dyson V11 Пилосос", 16499));
         cart.add(new Item(9, "Samsung ME83K Мікрохвильова піч", 3199));
         cart.add(new Item(10, "Philips DryCare Фен", 1499));
-
         System.out.println(cart);
-
         cart.removeById(9);
-
         System.out.println(cart);
+        System.out.println();
 
         Order order = new Order(1L, "John");
         String bill = order.formOrderBill(cart);
-        System.out.println(bill);
+        System.out.println(bill + "\n");
+
+        IntStack stack = new IntStack();
+        stack.push(23);
+        stack.push(-1);
+        System.out.println(stack.size());
+        System.out.println(stack.isEmpty());
+        stack.pop();
+        System.out.println(stack.peek());
+        stack.clear();
     }
 }
--- src/main/java/org/example/task2/Order.java
+++ src/main/java/org/example/task2/Order.java
@@ -2,32 +2,51 @@ package org.example.task2;
 
 public class Order {
 
-    public long id;
-    public String customer;
+    private long id;
+    private String customer;
 
     public Order(long id, String customer) {
         this.id = id;
         this.customer = customer;
     }
 
+    public long getId() {
+        return this.id;
+    }
+
+    public String getCustomer() {
+        return this.customer;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
+
     public String formOrderBill(Cart cart) {
+        if (cart.isEmpty()) {
+            return "Order number " + this.id + " for customer " + this.customer + "\nCart is empty!";
+        }
 
         StringBuilder builder = new StringBuilder();
-        builder.append("Order number ").append(id).append(" for customer ").append(customer);
+        builder.append("Order number ").append(this.id).append(" for customer ").append(this.customer);
         builder.append("\n------------------\n");
 
         double sum = 0.0;
 
-        for (int i = 0; i < cart.index; i++) {
+        for (int i = 0; i < cart.getCurrentSize(); i++) {
 
-            sum += cart.contents[i].price;
+            sum += cart.getItemAt(i).getPrice();
 
             builder.append("Item id: ");
-            builder.append(cart.contents[i].id);
+            builder.append(cart.getItemAt(i).getId());
             builder.append(" name: ");
-            builder.append(cart.contents[i].name);
+            builder.append(cart.getItemAt(i).getName());
             builder.append(" price: ");
-            builder.append(cart.contents[i].price);
+            builder.append(cart.getItemAt(i).getPrice());
             builder.append("\n");
         }
 
